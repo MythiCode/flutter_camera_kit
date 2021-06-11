5ae880a319262b978cb95c1b8e8475da4ee3a9fa
hrshs <hrshs91@gmail.com>
2021-06-11 16:04:33 +0430

    Migrate to null safety

--- lib/camerakit.dart
+++ lib/camerakit.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class Camerakit {
   static const MethodChannel _channel = const MethodChannel('camerakit');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
